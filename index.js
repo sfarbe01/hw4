d885e7d19fdbf2856646dc0afe6ef4cb7ab6061e
steph <steph.farber14@gmail.com>
2017-05-03 21:32:59 -0700

    fixess

--- index.js
+++ index.js
@@ -1,59 +1,18 @@
-/*
-var x = document.getElementsByClassName("p");
-var defaultVal = x.defaultValue;
-var currentVal = x.value;
-console.log(defaultVal);
-console.log(currentVal);
-
-
- $(elem).find("p.releasedate").html(movie.releasedate);
-
-
-$( "input[type=radio][name=gender]:checked" ).val();
-console.log(defaultVal);
-
-*/
-/*
 $(document).ready(function(){
-  $("input").keyup(function() {
+ $("input").change(function() {
+     if ($(this).attr('name') == 'gender') {
+    var input_id = $(this).attr("name");
+    var input_name = input_id.split("-")[0];
+    var output_id = input_name + "-output";
     var value = $( this ).val();
-    $( ".search" ).text( value );
-  })
-  .keyup(event); 
+    $("#" + output_id).text( value );   
+     } else {
+    var input_id = $(this).attr("id");
+    var input_name = input_id.split("-")[0];
+    var output_id = input_name + "-output";
+    var value = $( this ).val();
+    $("#" + output_id).text( value );   
+     }
+  });
 })
 
-$(document).ready(function(){
-  $("input").keyup(function() {
-    var value = $( this ).val();
-    $( ".tel" ).text( value );
-  })
-  .keyup(event); 
-})
-*/
-
-$(document).ready(function(){
-  $("input").change(function() {
-    var value = $( this ).val();
-    $( ".tel" ).text( value );
-  })
-  .change(event); 
-})
-
-$(document).ready(function(){
-  $("input").change(function() {
-    var value = $( this ).val();
-    $( ".search" ).text( value );
-  })
-  .change(event); 
-})
-/*
-$(document).ready(function(){
-     alert($("input:text").val());
-    console.log(value)
-    });
-
-function showInput() {
-    
-}
-$(document).ready(function(){
-*/
